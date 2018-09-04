7ccdc318d4be0a726af153ec64d76216513caf82
Zabrina L <zabrinatan@hotmail.com>
2018-09-05 09:39:53 +1000

    added scroll easing on click of a link

--- js/main.js
+++ js/main.js
@@ -1,5 +1,17 @@
 $(document).ready(function (){
 
+  $('nav a').on('click', function(){
+
+    let target  = $( this );
+    var element = target.attr('href');
+
+    $('nav a').removeClass('active');
+    target.addClass('active');
+    $("body, html").animate({
+        scrollTop: $( element ).offset().top
+      }, 1000);
+
+  })
 
 
 
