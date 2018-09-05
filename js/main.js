774a3245fd798015b3de0c6eac6bcb8d1c860ad0
Zabrina L <zabrinatan@hotmail.com>
2018-09-05 20:43:02 +1000

    changed wording of about me and contacts in nav to contact

--- js/main.js
+++ js/main.js
@@ -1,20 +1,20 @@
-$(document).ready(function (){
-let video = 0;
+$(document).ready(function() {
+  let video = 0;
 
 
-  const checkSection = function(){
-    if($("#1").hasClass("active")===false){
+  const checkSection = function() {
+    if ($("#1").hasClass("active") === false) {
       $('nav').addClass("fixnav");
-    }else{
+    } else {
       $('nav').removeClass('fixnav');
     }
 
   }
 
-  $(window).scroll(function(){
-    scroll =  $(window).scrollTop()
+  $(window).scroll(function() {
+    scroll = $(window).scrollTop()
     console.log(scroll)
-    if(scroll > 620 && scroll < 1322) {
+    if (scroll > 620 && scroll < 1322) {
       $('nav').addClass("fixnav")
       $('#2').addClass('active');
       $('#1').removeClass('active');
@@ -22,7 +22,7 @@ let video = 0;
       $('#4').removeClass('active');
       $('#5').removeClass('active');
 
-    }else{
+    } else {
       $('nav').removeClass('fixnav')
       $('#1').addClass('active');
       $('#2').removeClass('active');
@@ -32,21 +32,21 @@ let video = 0;
 
     }
 
-    if(scroll > 1322 && scroll < 2015) {
+    if (scroll > 1322 && scroll < 2015) {
       $('nav').addClass("fixnav")
       $('#3').addClass('active');
       $('#2').removeClass('active');
       $('#4').removeClass('active');
       $('#5').removeClass('active');
       $('#1').removeClass('active');
-    }else if (scroll > 2015 && scroll < 2715) {
+    } else if (scroll > 2015 && scroll < 2715) {
       $('nav').addClass("fixnav")
       $('#4').addClass('active');
       $('#3').removeClass('active');
       $('#5').removeClass('active');
       $('#2').removeClass('active');
       $('#1').removeClass('active');
-    } else if(scroll > 2715){
+    } else if (scroll > 2715) {
       $('nav').addClass("fixnav")
       $('#5').addClass('active');
       $('#4').removeClass('active');
@@ -58,42 +58,42 @@ let video = 0;
   })
 
 
-  $('nav a').on('click', function(e){
-      e.preventDefault();
-    let target  = $( this );
+  $('nav a').on('click', function(e) {
+    e.preventDefault();
+    let target = $(this);
     var element = target.attr('href');
 
     $('nav a').removeClass('active');
     target.addClass('active');
     $("body, html").animate({
-        scrollTop: $( element ).offset().top
-      }, 500);
+      scrollTop: $(element).offset().top
+    }, 500);
 
 
-      checkSection();
+    checkSection();
 
   })
 
-  $('#arrow').on('click', function(){
+  $('#arrow').on('click', function() {
 
     $("body, html").animate({
-        scrollTop: $( "#section_2" ).offset().top
-      }, 500);
-      $("#1").removeClass('active');
-      $("#2").addClass('active');
+      scrollTop: $("#section_2").offset().top
+    }, 500);
+    $("#1").removeClass('active');
+    $("#2").addClass('active');
 
-      checkSection();
+    checkSection();
 
 
 
   })
 
-  $('#section_3 video').mouseover(function(){
-      $(this).get(0).play();
+  $('#section_3 video').mouseover(function() {
+    $(this).get(0).play();
   })
 
-  $('#section_3 video').mouseout(function(){
-      $(this).get(0).pause();
+  $('#section_3 video').mouseout(function() {
+    $(this).get(0).pause();
   })
 
 
@@ -106,7 +106,7 @@ let video = 0;
 
   })
 
-  $("#resume").click(function(){
+  $("#resume").click(function() {
     window.open('https://drive.google.com/file/d/1rcd6DzegZ7MW1hNsaC-NTaHQASl4xBVw/view')
   })
 
